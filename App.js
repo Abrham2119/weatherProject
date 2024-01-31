ac5b4f27a6e9c43bf20639496600ed0e0d8022c9
Abrham2119 <abrhamzewdu2119.com>
2024-02-01 02:42:39 +0300

    i added  the get and post resposese from server

--- App.js
+++ App.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello!')
 })
 
-app.listen(3006, () => {
+app.get('/display', (req, res) => {
+    res.send(__dirname+'/index.html')
+})
+app.post('/display', (req, res) => {
+    const num1 = parseInt(req.body.num1)
+    res.send('The nunmber is '+num1)
+})
+
+app.listen(3008, () => {
   console.log('Example app listening on port 3000!')
 })
