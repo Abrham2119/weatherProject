01cec719cef24ced97deedfb4127df10c00560f1
Abrham2119 <abrhamzewdu2119.com>
2024-02-02 10:59:18 +0300

    i added the https

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
+const https = require('https');
+const { url } = require('inspector');
+
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
+
+  const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99#';
+
+  https.get(apiUrl, (response) => {
+    console.log(response);
+  });
+
   res.send('Hello!')
 })
 
-app.get('/display', (req, res) => {
-    res.sendFile(__dirname+'/index.html')
-})
-
-app.post('/display', function(req, res) {
-    console.log(num1)
-    var num1 =parseFloat(req.body.num1)
-    
-    res.send('Your BMI is '+num1)
-
-})
 
 app.listen(3008, () => {
   console.log('Example app listening on port 3000!')
